751f388969aafa1fa0475e7e9a70e157b42c8b7a
tharuAnish <namoanishtharu@gmail.com>
2023-07-26 15:45:05 +0530

    button styling

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -9,6 +9,8 @@ class GradientContainer extends StatelessWidget {
   final Color color1;
   final Color color2;
 
+  void rollDice() {}
+
   @override
   Widget build(context) {
     return Container(
@@ -23,11 +25,20 @@ class GradientContainer extends StatelessWidget {
         ),
       ),
       child: Center(
-          child: Column(children: [
+          child: Column(mainAxisSize: MainAxisSize.min, children: [
         Image.asset(
           'assets/images/dice-6.png',
           width: 200,
         ),
+        TextButton(
+            onPressed: rollDice,
+            style: TextButton.styleFrom(
+                padding: const EdgeInsets.only(
+                  top: 20,
+                ),
+                foregroundColor: Colors.white,
+                textStyle: const TextStyle(fontSize: 28)),
+            child: const Text('Roll Dice'))
       ])),
     );
   }
